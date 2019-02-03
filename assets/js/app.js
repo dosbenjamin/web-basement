60dce3b6b2dda163ffd57b8b95e6caaa2d2d4fab
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-02-03 19:14:08 +0100

    add mobile 2/2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,90 +1,52 @@
+// Navigation
 const burger = document.querySelector('.navBar__burger');
 const menu = document.querySelector('.menu');
 const menuLinks = document.querySelectorAll('.menu__link');
 const logo = document.querySelector('.navBar__logo');
+const menuList = document.querySelector('.menu__links');
 burger.addEventListener('click', (e) => {
     menu.classList.toggle('menu--open');
+    burger.classList.toggle('navBar__burger--open');
+    menuList.classList.toggle('menu__links--open');
 });
 logo.addEventListener('click', (e) => {
-    menu.classList.remove('menu--open');   
+    menu.classList.remove('menu--open'); 
+    burger.classList.remove('navBar__burger--open');
+    menuList.classList.remove('menu__links--open');
 });
 menuLinks.forEach(element => {
     element.addEventListener('click', (e) => {
-        menu.classList.remove('menu--open');   
+        menu.classList.remove('menu--open');
+        burger.classList.remove('navBar__burger--open');
+        menuList.classList.remove('menu__links--open');
     });
 });
 // Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
-	anchor.addEventListener('click', function () {
-		smoothScroll.scrollTo(this.getAttribute('href'), 1000);
-	});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth', block: 'start'
+        });
+    });
 });
-(function (window) {
-	'use strict';
-	var document = window.document;
-	var body = document.body;
-	var rootElement = document.documentElement;
-	var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function (func) {
-		window.setTimeout(func, 15);
-	};
-	var clock = '';
-	var time = 500;
-	var context = window;
-	var start = context.scrollTop || window.pageYOffset;
-	var end = 0;
-	var easeInOutCubic = function (t) {
-		return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-	};
-	var getTargetTop = function (target) {
-		var targetElement = {};
-		if (typeof target === 'number') {
-			return target;
-		} else if (typeof target === 'string') {
-			targetElement = document.querySelector(target);
-			if (!targetElement) {
-				return false;
-			}
-			return targetElement.getBoundingClientRect().top + window.pageYOffset;
-		}
-		return false;
-	};
-	var getScrollTop = function (startV, endV, elapsed, duration) {
-		if (elapsed > duration) {
-			return endV;
-		}
-		return startV + (end - startV) * easeInOutCubic(elapsed / duration);
-	};
-	var getScrollPageBottom = function () {
-		var contentHeight = Math.max.apply(null, [body.clientHeight, body.scrollHeight, rootElement.scrollHeight, rootElement.clientHeight]);
-		return contentHeight - window.innerHeight;
-	};
-	var scrollFrame = function () {
-		var elapsed = Date.now() - clock;
-		if (context === window) {
-			window.scroll(0, getScrollTop(start, end, elapsed, time));
-		} else {
-			context.scrollTop = getScrollTop(start, end, elapsed, time);
-		}
-		if (elapsed <= time) {
-			requestAnimationFrame(scrollFrame);
-		}
-	};
-	var SmoothScroll = function SmoothScroll() {};
-	SmoothScroll.prototype = {
-		scrollTo: function (target, duration, root) {
-			clock = Date.now();
-			time = duration || 500;
-			context = root || window;
-			start = context.scrollTop || window.pageYOffset;
-			end = getTargetTop(target);
-			scrollFrame();
-		},
-		scrollTop: function (duration, root) {
-			this.scrollTo(0, duration, root);
-		},
-		scrollBottom: function (duration, root) {
-			this.scrollTo(getScrollPageBottom(), duration, root);
-		}
-	};
-	window.smoothScroll = new SmoothScroll();
-}(window));
+// Timer
+function count() {
+    now = new Date();
+    now = new Date(Date.UTC(now.getFullYear(),now.getMonth(), now.getDate() , 
+    now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()));
+    futur = new Date(Date.UTC(2019, 4, 5, 12, 00)).getTime();
+    eventTime = Math.floor((futur - now) / 1000);
+    if (eventTime > 0) {
+        let days = parseInt(eventTime / 86400);
+        let hours = parseInt((eventTime - days * 86400) / 3600);
+        let minutes = parseInt((eventTime - (days * 86400) - (hours * 3600)) / 60);
+        let seconds = parseInt((eventTime - (days * 86400) - (hours * 3600) - (minutes * 60)));
+        document.querySelector('.landing .tiny--smaller').innerHTML = days + 'D ' + hours + 'H ' + minutes + 'M ' + seconds + 'S';
+        tempoCount = setTimeout("count()", 1000)
+    }
+    else {
+        clearTimeout(tempoCount);
+    }
+}
+window.onload = count;
