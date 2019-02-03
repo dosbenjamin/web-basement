0b3a5eb213aad951d6d6fbeb85a0631ace374aaf
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-02-03 22:22:20 +0100

    add tablet 1/2

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,14 +35,14 @@ function count() {
     now = new Date();
     now = new Date(Date.UTC(now.getFullYear(),now.getMonth(), now.getDate() , 
     now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()));
-    futur = new Date(Date.UTC(2019, 4, 5, 12, 00)).getTime();
+    futur = new Date(Date.UTC(2019, 3, 30, 12, 00)).getTime();
     eventTime = Math.floor((futur - now) / 1000);
     if (eventTime > 0) {
         let days = parseInt(eventTime / 86400);
         let hours = parseInt((eventTime - days * 86400) / 3600);
         let minutes = parseInt((eventTime - (days * 86400) - (hours * 3600)) / 60);
         let seconds = parseInt((eventTime - (days * 86400) - (hours * 3600) - (minutes * 60)));
-        document.querySelector('.landing .tiny--smaller').innerHTML = days + 'D ' + hours + 'H ' + minutes + 'M ' + seconds + 'S';
+        document.querySelector('.landing .tiny--smaller').innerHTML = days + 'J ' + hours + 'H ' + minutes + 'M ' + seconds + 'S';
         tempoCount = setTimeout("count()", 1000)
     }
     else {
